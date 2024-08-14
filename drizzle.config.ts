439ae54216099d6b11476d35c4ab8111d9d87ca0
shmmh <shammah.rashad@gmail.com>
2024-08-14 23:00:35 +0100

    added nextui and drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig, type Config } from "drizzle-kit";
 import { loadEnvConfig } from "@next/env";
+import cfg from "@/lib/config";
 
 const projectDir = process.cwd();
 loadEnvConfig(projectDir);
 
-const config = {
+const config: Config = {
   schema: "./src/lib/schema.ts",
   out: "./drizzle",
-  dialect: "postgresql",
+  dialect:"postgresql",
   dbCredentials: {
-    url: process.env.POSTGRES_URL as string,
+    url: cfg.POSTGRES_URL as string,
   },
-} satisfies Config;
+}
 
 export default defineConfig(config);
